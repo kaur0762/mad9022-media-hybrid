05d6441ae76ddc36054e63910c137bc0426c1018
kaur0762 <kaur0762@algonquinlive.com>
2022-01-31 14:43:49 -0500

    array songlist created

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const array = [
+    {
+        id: 0, name: "February Song" ,artist: "Joshan Groban",  audio: "./media/february-song.mp3", image:"./media/february-song.jpg" 
+    },
+    {
+        id: 1, name: "Ghost" ,artist: "Justin Bieber",  audio: "./media/ghost.mp3", image:"./media/ghost.jpg" 
+    },
+    {
+        id: 2, name: "MONTERO" ,artist: "Lil Nias X",  audio: "./media/montero.mp3", image:"./media/montero.jpg" 
+    },
+    {
+        id: 3, name: "White Inverson" ,artist: "Post Malone",  audio: "./media/white-inverson.mp3", image:"./media/white-inverson.jpg" 
+    },
+    {
+        id: 4, name: "Yummy" ,artist: "Justin Bieber",  audio: "./media/yummy.mp3", image:"./media/yummy.jpg" 
+    }
+]
